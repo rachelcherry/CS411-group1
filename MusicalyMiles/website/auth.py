bfd9fee7a405af854fecc0107244d49d157151e1
mlrivera123 <mlrivera@bu.edu>
2023-11-29 12:33:09 -0500

    added syntax fix

--- MusicalyMiles/website/auth.py
+++ MusicalyMiles/website/auth.py
@@ -1,26 +1,30 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
+
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        user = User.query.filter_by(email = email).first()
+
+        user = User.query.filter_by(email=email).first()
         if user:
             if user.password == password:
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('incorrect password', category='error')
+                flash('Incorrect password, try again.', category='error')
         else:
-            flash('email does not exist', category='error')
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html", user=current_user)
 
 
@@ -30,6 +34,7 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
+
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
@@ -38,22 +43,22 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email = email).first()
+        user = User.query.filter_by(email=email).first()
         if user:
-            flash('Email already exists', category="error")
+            flash('Email already exists.', category='error')
         elif len(email) < 4:
-            flash('Email must be greater than 3 characters', category='error')
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 characters', category='error')
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords do not match', category='error')
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            flash('Password must be at least 7 characters', category='error')
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=password1)            
+            new_user = User(email=email, first_name=first_name, password= password1)
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
