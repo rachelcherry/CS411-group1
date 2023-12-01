df5e78a61db80375de6a736a54ac7bee603d0498
Crosby Nash <crosbyn@bu.edu>
2023-12-01 08:55:25 -0500

    update

--- MusicalyMiles/website/views.py
+++ MusicalyMiles/website/views.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-
+from website.config import SPOTIFY_API_KEY
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    return render_template("home.html", user=current_user, spotify_api_key=SPOTIFY_API_KEY)
