f7d875131fd4529a78dc64fbc03052c5781eddb7
Inhye Kang <inhye@bu.edu>
2023-12-07 20:10:42 -0500

    move styles into static

--- MusicalyMiles/website/__init__.py
+++ MusicalyMiles/website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "dpipatabase.db"
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     app.config['SECRET_KEY'] = '123456789'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
