7af8458f67e196fd41a0b62835965da800500d47
Inhye Kang <inhye@bu.edu>
2023-12-07 20:25:21 -0500

    add css directory in static; import render_temp in init

--- MusicalyMiles/website/__init__.py
+++ MusicalyMiles/website/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import request
 from os import path
 from flask_login import LoginManager
+from flask import render_template
 
 
 db = SQLAlchemy()
